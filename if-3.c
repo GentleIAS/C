49046400f14465b8fa736f43e16443ca59d21277
GentleIAS <155397073+GentleIAS@users.noreply.github.com>
2024-07-03 22:11:01 +0800

    从键盘输入一个字符对其转换
    从键盘输入一个字符，如果字符为大写字母则转换为小写输出，如果为小写字母则转换为大写输出，不是则不转换输出。

---
+++ if-3.c
@@ -0,0 +1,22 @@
+/*
+从键盘输入一个字符，如果字符为大写字母则转换为小写输出，如果为小写字母则转换为大写输出，不是则不转换输出。
+*/
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("请输入一个字符：");
+    scanf("%c",&c);
+    if(c>='a'&&c<='z')
+    {
+        printf("%c\n",c-32);
+    }
+    else if(c>='A'&&c<='Z')
+    {
+        printf("%c\n",c+32);
+    }
+    else
+    {
+        printf("%c\n",c);
+    }
+}
