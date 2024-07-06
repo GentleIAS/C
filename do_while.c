cf8a003596a10617fe59cb211dfdb708f26d72b5
GentleIAS <155397073+GentleIAS@users.noreply.github.com>
2024-07-06 20:26:13 +0800

    从键盘输入一个整数n，计算n的阶乘（使用do while）

---
+++ do_while.c
@@ -0,0 +1,16 @@
+/*
+从键盘输入一个整数n，计算n的阶乘（使用do while）
+*/
+#include<stdio.h>
+int main()
+{
+    int i=1,n,s=1;
+    printf("请输入一个整数：");
+    scanf("%d",&n);
+    do
+    {
+        s=s*i;
+        i++;
+    } while (i<=n);
+    printf("%d的阶乘为%d\n",n,s);
+}
