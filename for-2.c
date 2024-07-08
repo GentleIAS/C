c56488b5566de1a27011f1bc19c55bf70ef9c629
GentleIAS <155397073+GentleIAS@users.noreply.github.com>
2024-07-08 14:19:42 +0800

    从键盘输入一个整数，判断该数是否为素数

--- for-2.c
+++ for-2.c
@@ -4,20 +4,19 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int i,n;
     printf("请输入一个整数：");
     scanf("%d",&n);
-    for(int i=2;i<n;i++)
+    for(i=2;i<n;i++)
     {
         if(n%i==0)
         {
             printf("%d是不素数\n",n);
             break;
         }
-        else
-        {
-            printf("%d是素数\n",n);
-            break;
-        }
+    }
+    if(n==i)
+    {
+        printf("%d是素数\n",n);
     }
 }
